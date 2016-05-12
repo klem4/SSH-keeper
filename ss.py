0234a43359cd9c044597277428133a3d64630bfc
klem4 <useperlordie@gmail.com>
2016-05-12 15:28:10 +0300

    in progress

--- ss.py
+++ ss.py
@@ -5,21 +5,26 @@ import sqlite3
 
 
 class SSMemory(object):
+    db_name = 'ss.db'
+    tb_name = 'ss_hosts'
+
     def __init__(self, name_or_part):
         self.name_or_part = name_or_part
-        self.conn = sqlite3.connect('ss.db')
+        self.conn = sqlite3.connect(self.db_name)
         self.ensure_table()
 
     def ensure_table(self):
         self.conn.execute(
-            "CREATE TABLE IF NOT EXISTS ss_hosts ("
+            "CREATE TABLE IF NOT EXISTS %s ("
             "id INTEGER PRIMARY KEY AUTOINCREMENT,"
             "connection_data TEXT NOT NULL"
-            ")"
-        )
+            ")" % self.tb_name)
 
     def get_candidates(self):
         c = self.conn.cursor()
+        c.execute(
+            "SELECT connection_data cd FROM %s WHERE cd LIKE ?" %
+            self.tb_name, ("'%%%s%%'" % self.name_or_part,))
 
 
 class SSChooser(object):
