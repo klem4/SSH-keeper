10f156f14ce542581e756d10ce45a37d3f79e90b
Andrey Klimenko <klem4@MacBook-Pro-Andrey.local>
2016-06-06 23:14:38 +0300

    tests in progress

--- tests.py
+++ tests.py
@@ -1,11 +1,28 @@
 # coding: utf-8
 
-from unittest import TestCase
+import os
+import sqlite3 from unittest import TestCase
+from ssh_keeper import SKChooser, SKMemory
 
 
 class CommonTestCase(TestCase):
+    db_name = SKMemory.db_name
+    table_name = SKMemory.db_name
+
     def setUp(self):
         pass
 
+    def tearDown(self):
+        try:
+            os.path.unlink(self.db)
+        except:
+            pass
+
+    def testNoCandidates(self):
+        pass
+
     def testAddHost(self):
+        pass
+
+    def testRemoveHost(self):
         pass
